049896e206c77c44caf6c157fd850013c88a14cc
Adam Franks <52870365+adfranks@users.noreply.github.com>
2019-09-10 14:59:54 -0400

    Update brickball.php

--- brickball.php
+++ brickball.php
@@ -23,7 +23,7 @@ if (!isset($_SESSION['uname'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="pragma" content="no-cache">
   <link rel="icon" href="images/brickball-icon.png">
-  <link rel="stylesheet" type="text/css" href="css/brickball.css">
+  <link rel="stylesheet" type="text/css" href="brickball.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
@@ -237,7 +237,7 @@ if (!isset($_SESSION['uname'])) {
     </footer>
   </div>
 
-  <script src="js/brickball.js"></script>
+  <script src="brickball.js"></script>
 
 </body>
 
