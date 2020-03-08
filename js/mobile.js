593b3beeddd03e7c0117bad188846901edb8d724
Adam Franks <52870365+adfranks@users.noreply.github.com>
2020-03-08 13:29:07 -0400

    Add files via upload

---
+++ js/mobile.js
@@ -0,0 +1,6 @@
+// Create pop-up that alerts user if device is too small.
+function mobile() {
+  if (screen.width < 900) {
+    alert("To play this game, try a larger device and have fun.");
+  } 
+} 
