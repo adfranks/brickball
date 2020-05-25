d2df684c6bd2098c492ff185540482bd71deb8f4
Adam Franks <52870365+adfranks@users.noreply.github.com>
2020-05-25 06:44:33 -0400

    24 hour password reset

--- maillink.php
+++ maillink.php
@@ -5,7 +5,8 @@
   <meta charset="UTF-8" />
   <title>Brick ball</title>
   <meta name="keywords" content="brick ball forgot password" />
-  <meta name="description" content="breakout game for web browser password reset instructions" />
+  <meta name="description" content="breakout game for web browser password 
+  reset instructions" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="css/brickball.css">
 </head>
@@ -20,13 +21,10 @@
     $password = "gosun";
     $dbname = "adpfrank_db1";
     $token = bin2hex(openssl_random_pseudo_bytes(16));
-    $expiry = strtotime("+1 hour");
+    $expiry = strtotime("+24 hour");
     $email = $_POST['email'];
     $url = "<a href='adfranks.com/brickball/updatepwd.php?token=" . $token . "&email=" . $email . "'>Reset Password</a>";
-    $msg = "<html><head><title>Reset Password</title></head>
-            <body><p>If you requested to reset your password, please click on 
-            the link within the hour:  " . $url . "</p>
-            <p>Otherwise, ignore this message.<br />Brickball</p></body></html>";
+    $msg = "<html><head><title>Reset Password</title></head><body><p>If you requested to reset your password, please click on the link within the hour:  " . $url . "</p><p>Otherwise, ignore this message.<br />Brickball</p></body></html>"; 
     $headers = "MIME-Version: 1.0" . "\r\n";
     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
     $headers .= "From: info@adfranks.com" . "\r\n";
@@ -38,18 +36,15 @@
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         // sql to find user and put the token and expiration in the db
-        $sql = $conn->prepare("UPDATE members SET token='" . $token . "',
-               expiration='" . $expiry . "' WHERE email='" . $email . "'");
+        $sql = $conn->prepare("UPDATE members SET token='" . $token . "', expiration='" . $expiry . "' WHERE email='" . $email . "'");
+
         $sql->execute();
         
         // mail a link to user with token attached to url, then give a success response with further instructions
         mail($email,"Reset Password",$msg,$headers);
-        echo '<p class="form-response">A message was just sent to your email 
-              address.  Within the hour, open the message and click on
-              the provided link to reset your password.</p>';
+        echo '<p class="form-response">A message was just sent to your email address.  Within 24 hours, open the message and click on the provided link to reset your password.</p>';
     } catch (PDOException $e) {
-        echo '<span class="errormsg">Oopsy Daisy!  Error: ' . $e->getMessage() . 
-        '</span><br /><a href="brickball.php">Go back</a>.';
+        echo '<span class="errormsg">Oopsy Daisy!  Error: ' . $e->getMessage() . '</span><br /><br /><a href="brickball.php">Go back to the game</a>.';
     }
 
     $conn = null;
